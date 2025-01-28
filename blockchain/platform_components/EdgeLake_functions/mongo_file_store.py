2a75ab5965d0c78db801f80acd3a16b139f79c2f
royshadmon <16313057+royshadmon@users.noreply.github.com>
2025-01-28 08:08:58 -0800

    read file from mongo using run client

--- blockchain/platform_components/EdgeLake_functions/mongo_file_store.py
+++ blockchain/platform_components/EdgeLake_functions/mongo_file_store.py
@@ -23,7 +23,7 @@ def write_file(edgelake_node_url, dbms, table, filename):
         'Content-Type': 'application/octet-stream',  # Specify binary content
         'command': f'file store where dbms = {dbms} and table = {table} and dest = {filename.split("/")[-1]}'
     }
-    
+
     with open(filename, 'rb') as f:
         binary_data = f.read()
         response = requests.post(edgelake_node_url, headers=headers, data=binary_data)
@@ -41,10 +41,12 @@ def read_file(edgelake_node_url, dbms, table, filename, dest, ip_port):
     headers = {
         'User-Agent': 'AnyLog/1.23',
         'Content-Type': 'text/plain',
-        'command': f'file get (dbms={dbms} and table={table} and id={filename.split("/")[-1]}) {dest}',
-        'destination': ip_port
+        # 'command': f'file get (dbms={dbms} and table={table} and id={filename.split("/")[-1]}) {dest}',
+        'command': f'run client {ip_port} file get (dbms={dbms} and table={table} and id={filename.split("/")[-1]}) {dest}'
+        # 'destination': ip_port
     }
 
+    print(f"FILE GET COMMAND: headers: {headers['command']}")
     try:
         response = requests.post(edgelake_node_url, headers=headers, data='')
         return response
@@ -55,6 +57,7 @@ def read_file(edgelake_node_url, dbms, table, filename, dest, ip_port):
 
 if __name__ == '__main__':
     file_write_destination = os.getenv("FILE_WRITE_DESTINATION")
-    write_file(f"http://192.168.1.118:32049", "blobs_admin", "my_table",
-               f"{file_write_destination}/node1/1-replica-node1.pkl")
-    read_file(f"http://192.168.1.118:32049", "blobs_admin", "my_table", "../../1-replica-node1.pkl", f"{file_write_destination}/aggregator/1-replica-node1.pkl", None)
+    # write_file(f"http://192.168.1.118:32049", "blobs_admin", "my_table",
+    #            f"{file_write_destination}/node1/1-replica-node1.pkl")
+    response = read_file(f"http://192.168.1.118:32048", "blobs_winniio_fl", "node_model_updates", "1-replica-node3.pkl", f"{file_write_destination}/aggregator/1-replica-node1.pkl", "192.53.121.36:32148")
+    print(response.status_code)
